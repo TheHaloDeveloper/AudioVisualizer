139104714c5f8eb85786db8b6a80c51b7b56bdb9
Halo <68120801+TheHaloDeveloper@users.noreply.github.com>
2025-04-16 21:52:57 -0700

    changed texture, orbit controls

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
+const FLOOR_SIZE = 100;
+
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 let renderer = new THREE.WebGLRenderer({antialias: true});
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById("scene").appendChild(renderer.domElement);
-camera.position.z = 5;
+camera.position.set(0, -14, 6);
+
+let controls = new THREE.OrbitControls(camera, renderer.domElement);
 
 let loader = new THREE.TextureLoader();
 let floorTexture = loader.load("images/grid.png");
-floorTexture.repeat.set(0.1, 0.2);
+floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping;
+floorTexture.repeat.set(0.1 * FLOOR_SIZE, 0.1 * FLOOR_SIZE);
 
 let ambient = new THREE.AmbientLight(0xffffff, 1);
 scene.add(ambient);
 
 let box = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.BoxGeometry(FLOOR_SIZE, FLOOR_SIZE, 1),
     new THREE.MeshBasicMaterial({color: 0xffffff, map: floorTexture})
 )
 scene.add(box);
